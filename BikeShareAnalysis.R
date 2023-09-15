323d2e093c08668d6563b270b3acb666dd89ed2b
Maddie Portrey <maddiekay13@gmail.com>
2023-09-15 14:33:56 -0600

    predictions code

--- BikeShareAnalysis.R
+++ BikeShareAnalysis.R
@@ -7,21 +7,34 @@ bike <- vroom("./train.csv")
 new <- vroom("test.csv")
 
 bike <- bike %>%
-  mutate(weather = ifelse(weather == 4,3,weather)) %>%
   select(-casual, -registered)
 
 #perform at least 2 feature engineering steps using recipe (change to factors, create time of day variable)
 
 my_recipe <- recipe(count ~ ., data = bike) %>%
+  step_mutate(weather = ifelse(weather == 4,3,weather)) %>%
   step_num2factor(weather, levels = c('1','2','3','4')) %>% #change to factor
   step_num2factor(season, levels = c('0','1')) %>% #change to factor
   step_num2factor(holiday, levels = c('0','1'), transform = function(x) x + 1) %>% #change to factor
   step_num2factor(workingday, levels = c('0','1'), transform = function(x) x + 1) %>% #change to factor
-  step_date(datetime, features = "dow") %>% #get day of the week
-  step_time(datetime, features = "hour") %>% #get hour of day
-  step_zv(all_predictors()) %>%
-  step_rm(datetime)
+  step_time(datetime, features = "hour") #get hour of day
   
 prepped_recipe <- prep(my_recipe)
 baked <- bake(prepped_recipe, new_data = bike)
-head(baked, 10)
+
+my_mod <- linear_reg() %>%
+  set_engine("lm")
+
+bike_workflow <- workflow() %>%
+  add_recipe(my_recipe) %>%
+  add_model(my_mod) %>%
+  fit(data = bike)
+
+bike_predictions <- predict(bike_workflow, new_data = new)
+
+bike_predictions$.pred[is.na(bike_predictions$.pred)] <- 0
+bike_predictions$.pred[bike_predictions$.pred < 0] <- 0
+preds <- cbind(new$datetime, bike_predictions)
+colnames(preds) <- c("datetime","count")
+
+vroom_write(preds, "predictions.csv", ",")
