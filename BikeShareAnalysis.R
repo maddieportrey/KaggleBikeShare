dd3f23d8f00ad2bc3d6e9fac318bef2bade209db
Maddie Portrey <maddiekay13@gmail.com>
2023-09-18 13:30:14 -0600

    poisson model code and csv

--- BikeShareAnalysis.R
+++ BikeShareAnalysis.R
@@ -3,38 +3,76 @@ library(vroom)
 library(tidymodels)
 setwd("./KaggleBikeShare")
 
-bike <- vroom("./train.csv")
-new <- vroom("test.csv")
+#read in csvs
+bikeTrain <- vroom("./train.csv")
+bikeTest <- vroom("test.csv")
 
-bike <- bike %>%
+#remove casual and registered from train, not included in test
+bikeTrain <- bikeTrain %>%
   select(-casual, -registered)
 
-#perform at least 2 feature engineering steps using recipe (change to factors, create time of day variable)
-
-my_recipe <- recipe(count ~ ., data = bike) %>%
+#create recipe for data (both train and test)
+my_recipe <- recipe(count ~ ., data = bikeTrain) %>%
   step_mutate(weather = ifelse(weather == 4,3,weather)) %>%
   step_num2factor(weather, levels = c('1','2','3','4')) %>% #change to factor
   step_num2factor(season, levels = c('0','1')) %>% #change to factor
   step_num2factor(holiday, levels = c('0','1'), transform = function(x) x + 1) %>% #change to factor
   step_num2factor(workingday, levels = c('0','1'), transform = function(x) x + 1) %>% #change to factor
-  step_time(datetime, features = "hour") #get hour of day
+  step_time(datetime, features = "hour") %>% #get hour of day
+  step_rm(datetime)
   
 prepped_recipe <- prep(my_recipe)
-baked <- bake(prepped_recipe, new_data = bike)
+baked <- bake(prepped_recipe, new_data = bikeTrain)
 
 my_mod <- linear_reg() %>%
   set_engine("lm")
 
+#set up the workflow
 bike_workflow <- workflow() %>%
   add_recipe(my_recipe) %>%
   add_model(my_mod) %>%
-  fit(data = bike)
+  fit(data = bikeTrain)
 
-bike_predictions <- predict(bike_workflow, new_data = new)
+#get predictions for test set
+bike_predictions <- predict(bike_workflow, new_data = bikeTest)
+
+#format for kaggle
+bike_predictions$.pred[is.na(bike_predictions$.pred)] <- 0
+bike_predictions$.pred[bike_predictions$.pred < 0] <- 0
+
+#bind predictions and datetime column, format for kaggle
+preds <- cbind(bikeTest$datetime, bike_predictions)
+colnames(preds) <- c("datetime","count")
+preds$datetime <- as.character(format(preds$datetime))
+
+#write to csv
+vroom_write(preds, "predictions.csv", ",")
+
+#look at the fitted LM model here
+extract_fit_engine(bike_workflow) %>%
+  tidy()
+
+#fit a poisson model
+library(poissonreg)
+
+pois_mod <- poisson_reg() %>%
+  set_engine("glm")
+
+bike_pois_workflow <- workflow() %>%
+  add_recipe(my_recipe) %>%
+  add_model(pois_mod) %>%
+  fit(data = bikeTrain)
+
+bike_predictions <- predict(bike_pois_workflow,
+                            new_data = bikeTest)
 
 bike_predictions$.pred[is.na(bike_predictions$.pred)] <- 0
 bike_predictions$.pred[bike_predictions$.pred < 0] <- 0
-preds <- cbind(new$datetime, bike_predictions)
-colnames(preds) <- c("datetime","count")
 
-vroom_write(preds, "predictions.csv", ",")
+#bind predictions and datetime column, format for kaggle
+preds <- cbind(bikeTest$datetime, bike_predictions)
+colnames(preds) <- c("datetime","count")
+preds$datetime <- as.character(format(preds$datetime))
+
+#write to csv
+vroom_write(preds, "pois_predictions.csv", ",")
