6e7131ad785e37b98292af9c86f7663fe638ef8e
Maddie Portrey <maddiekay13@gmail.com>
2023-09-25 13:46:26 -0600

    tuning parameters

--- BikeShareAnalysis.R
+++ BikeShareAnalysis.R
@@ -1,6 +1,8 @@
 library(tidyverse)
 library(vroom)
 library(tidymodels)
+library(glmnet)
+library(poissonreg)
 setwd("./KaggleBikeShare")
 
 #read in csvs
@@ -12,13 +14,13 @@ bikeTrain <- bikeTrain %>%
   select(-casual, -registered)
 
 #create recipe for data (both train and test)
-my_recipe <- recipe(count ~ ., data = bikeTrain) %>%
-  step_mutate(weather = ifelse(weather == 4,3,weather)) %>%
-  step_num2factor(weather, levels = c('1','2','3','4')) %>% #change to factor
-  step_num2factor(season, levels = c('0','1')) %>% #change to factor
-  step_num2factor(holiday, levels = c('0','1'), transform = function(x) x + 1) %>% #change to factor
-  step_num2factor(workingday, levels = c('0','1'), transform = function(x) x + 1) %>% #change to factor
-  step_time(datetime, features = "hour") %>% #get hour of day
+my_recipe <- recipe(count~., data=bikeTrain) %>%
+  step_mutate(weather=ifelse(weather==4, 3, weather)) %>% #Relabel weather 4 to 3
+  step_num2factor(weather, levels=c("Sunny", "Mist", "Rain")) %>%
+  step_num2factor(season, levels=c("Spring", "Summer", "Fall", "Winter")) %>%
+  step_mutate(holiday=factor(holiday, levels=c(0,1), labels=c("No", "Yes"))) %>%
+  step_mutate(workingday=factor(workingday,levels=c(0,1), labels=c("No", "Yes"))) %>%
+  step_time(datetime, features="hour") %>%
   step_rm(datetime)
   
 prepped_recipe <- prep(my_recipe)
@@ -76,3 +78,166 @@ preds$datetime <- as.character(format(preds$datetime))
 
 #write to csv
 vroom_write(preds, "pois_predictions.csv", ",")
+
+
+bikeTrain <- vroom("./train.csv")
+bikeTest <- vroom("test.csv")
+
+bikeTrain <- bikeTrain %>%
+  select(-casual, -registered)
+
+#do a linear regression on log counts
+#penalized regression on log count
+my_recipe <- recipe(count~., data=bikeTrain) %>%
+  step_mutate(weather=ifelse(weather==4, 3, weather)) %>% #Relabel weather 4 to 3
+  step_num2factor(weather, levels=c("Sunny", "Mist", "Rain")) %>%
+  step_num2factor(season, levels=c("Spring", "Summer", "Fall", "Winter")) %>%
+  step_mutate(holiday=factor(holiday, levels=c(0,1), labels=c("No", "Yes"))) %>%
+  step_mutate(workingday=factor(workingday,levels=c(0,1), labels=c("No", "Yes"))) %>%
+  step_time(datetime, features="hour") %>%
+  step_rm(datetime) %>%
+  step_dummy(all_nominal_predictors()) %>%
+  step_normalize(all_numeric_predictors())
+
+preg_model <- linear_reg(penalty = 4, mixture = .5) %>%
+  set_engine("glmnet")
+preg_wf <- workflow() %>%
+  add_recipe(my_recipe) %>%
+  add_model(preg_model) %>%
+  fit(data = bikeTrain)
+bike_predictions <- predict(preg_wf, new_data = bikeTest)
+bike_predictions$.pred[is.na(bike_predictions$.pred)] <- 0
+bike_predictions$.pred[bike_predictions$.pred < 0] <- 0
+
+#bind predictions and datetime column, format for kaggle
+preds <- cbind(bikeTest$datetime, bike_predictions)
+colnames(preds) <- c("datetime","count")
+preds$datetime <- as.character(format(preds$datetime))
+
+vroom_write(preds, "penalty_predictions.csv", ",")
+
+
+
+
+bikeTrain <- vroom("./train.csv")
+bikeTest <- vroom("test.csv")
+
+bikeTrain <- bikeTrain %>%
+  select(-casual, -registered)
+
+#bikeTrain$day <- wday(bikeTrain$datetime)
+#bikeTest$day <- wday(bikeTest$datetime)
+
+logBikeTrain <- bikeTrain %>%
+  mutate(count = log(count))
+
+bike_recipe <- recipe(count~., data=bikeTrain) %>%
+  step_mutate(weather=ifelse(weather==4, 3, weather)) %>% #Relabel weather 4 to 3
+  step_mutate(weather=factor(weather, levels=1:3, labels=c("Sunny", "Mist", "Rain"))) %>%
+  step_mutate(season=factor(season, levels=1:4, labels=c("Spring", "Summer", "Fall", "Winter"))) %>%
+  step_mutate(weekend = ifelse((workingday == 0 & holiday == 0), 1, 0)) %>%
+  step_mutate(holiday=factor(holiday, levels=c(0,1), labels=c("No", "Yes"))) %>%
+  step_mutate(workingday=factor(workingday,levels=c(0,1), labels=c("No", "Yes"))) %>%
+  step_mutate(weekend=factor(weekend, levels = c(0,1), labels = c("No", "Yes"))) %>%
+  step_time(datetime, features="hour") %>%
+  step_rm(workingday, datetime, atemp) %>%
+  step_dummy(all_nominal_predictors()) %>%
+  step_normalize(all_numeric_predictors())
+
+pois_mod <- poisson_reg() %>%
+  set_engine("glm")
+bike_pois_workflow <- workflow() %>%
+  add_recipe(bike_recipe) %>%
+  add_model(pois_mod) %>%
+  fit(data = bikeTrain)
+pois_bike_preds <- predict(bike_pois_workflow, new_data = bikeTest)
+
+preg_model <- poisson_reg(penalty = 5, mixture = .8) %>%
+  set_engine("glm")
+preg_wf <- workflow() %>%
+  add_recipe(bike_recipe) %>%
+  add_model(preg_model) %>%
+  fit(data = bikeTrain)
+preg_bike_preds <- predict(preg_wf, new_data = bikeTest)
+
+preds_function <- function(bike_predictions) {
+  bike_predictions$.pred[is.na(bike_predictions$.pred)] <- 0
+  bike_predictions$.pred[bike_predictions$.pred < 0] <- 0
+  preds <- cbind(bikeTest$datetime, bike_predictions)
+  colnames(preds) <- c("datetime","count")
+  preds$datetime <- as.character(format(preds$datetime))
+  #preds$count <- exp(preds$count)
+  
+  return(preds)
+}
+pois_preds <- preds_function(pois_bike_preds)
+preg_preds <- preds_function(preg_bike_preds)
+
+vroom_write(pois_preds, "pois_predictions.csv", ",")
+vroom_write(preg_preds, "preg_predictions.csv", ",")
+
+
+
+bikeTrain <- vroom("./train.csv")
+bikeTest <- vroom("test.csv")
+
+bikeTrain <- bikeTrain %>%
+  select(-casual, -registered)
+
+bikeTrain <- bikeTrain %>%
+  mutate(count = log(count))
+
+bike_recipe <- recipe(count~., data=bikeTrain) %>%
+  step_mutate(weather=ifelse(weather==4, 3, weather)) %>% #Relabel weather 4 to 3
+  step_mutate(weather=factor(weather, levels=1:3, labels=c("Sunny", "Mist", "Rain"))) %>%
+  step_mutate(season=factor(season, levels=1:4, labels=c("Spring", "Summer", "Fall", "Winter"))) %>%
+  step_mutate(weekend = ifelse((workingday == 0 & holiday == 0), 1, 0)) %>%
+  step_mutate(holiday=factor(holiday, levels=c(0,1), labels=c("No", "Yes"))) %>%
+  step_mutate(workingday=factor(workingday,levels=c(0,1), labels=c("No", "Yes"))) %>%
+  step_mutate(weekend=factor(weekend, levels = c(0,1), labels = c("No", "Yes"))) %>%
+  step_time(datetime, features="hour") %>%
+  step_rm(workingday, datetime, atemp) %>%
+  step_dummy(all_nominal_predictors()) %>%
+  step_normalize(all_numeric_predictors())
+
+preg_model <- linear_reg(penalty=tune(), mixture=tune()) %>%
+  set_engine("glmnet")
+
+preg_wf <- workflow() %>%
+  add_recipe(bike_recipe) %>%
+  add_model(preg_model)
+
+tuning_grid <- grid_regular(penalty(),
+                            mixture(),
+                            levels = 5)
+
+folds <- vfold_cv(bikeTrain, v = 30, repeats = 1)
+
+CV_results <- preg_wf %>%
+  tune_grid(resamples=folds,
+            grid=tuning_grid,
+            metrics=metric_set(rmse, mae, rsq))
+
+bestTune <- CV_results %>%
+  select_best("rmse")
+
+final_wf <- preg_wf %>%
+  finalize_workflow(bestTune) %>%
+  fit(data = bikeTrain)
+
+preds <- final_wf %>%
+  predict(new_data = bikeTest)
+
+preds_function <- function(bike_predictions) {
+  bike_predictions$.pred[is.na(bike_predictions$.pred)] <- 0
+  bike_predictions$.pred[bike_predictions$.pred < 0] <- 0
+  preds <- cbind(bikeTest$datetime, bike_predictions)
+  colnames(preds) <- c("datetime","count")
+  preds$datetime <- as.character(format(preds$datetime))
+  preds$count <- exp(preds$count)
+  
+  return(preds)
+}
+
+final_preds <- preds_function(preds)
+vroom_write(final_preds, "final_predictions.csv", ",")
